73fa6c4783410998d1e282076097f72cc35a5dd8
andrsbastardo <155968678+andrsbastardo@users.noreply.github.com>
2024-02-24 13:15:22 +0100

    toques finales

--- programar/Cursus/Cirulo0/libft/ft_bzero.c
+++ programar/Cursus/Cirulo0/libft/ft_bzero.c
@@ -6,11 +6,12 @@
 /*   By: abastard <abastard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/11 16:19:11 by usuadmin          #+#    #+#             */
-/*   Updated: 2024/02/24 12:51:45 by abastard         ###   ########.fr       */
+/*   Updated: 2024/02/24 13:12:26 by abastard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+/*void *s no posee un tipo de dato*/
 
 void	bzero(void *s, __SIZE_TYPE__ n)
 {
